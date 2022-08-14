a8ab9cb423484e2bb6db90332726e20c7c40d4d0
to-the-moon-1 <darkangeloflove99@gmail.com>
2022-08-14 05:22:50 +0300

    edited requirements

--- tic_tac_toe/settings.py
+++ tic_tac_toe/settings.py
@@ -75,10 +75,6 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
